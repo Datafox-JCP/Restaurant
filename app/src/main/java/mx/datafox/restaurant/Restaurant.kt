baabcb0053bf855dc574fe88a9d3a9f4ba13c5a9
Datafox-JCP <datafox@outlook.com>
2023-07-17 20:59:42 -0600

    - Jetpack ViewModel

--- app/src/main/java/mx/datafox/restaurant/Restaurant.kt
+++ app/src/main/java/mx/datafox/restaurant/Restaurant.kt
@@ -3,7 +3,8 @@ package mx.datafox.restaurant
 data class Restaurant(
     val id: Int,
     val title: String,
-    val description: String
+    val description: String,
+    var isFavorite: Boolean = false
 )
 
 val dummyRestaurants = listOf(
--- app/src/main/java/mx/datafox/restaurant/RestaurantsScreen.kt
+++ app/src/main/java/mx/datafox/restaurant/RestaurantsScreen.kt
@@ -1,15 +1,20 @@
 package mx.datafox.restaurant
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,26 +22,55 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import mx.datafox.restaurant.ui.theme.RestaurantTheme
 
 @Composable
 fun RestaurantsScreen() {
+
+    val viewModel: RestaurantsViewModel = viewModel()
+
     LazyColumn(
-        contentPadding = PaddingValues(
-            vertical = 8.dp,
-            horizontal = 8.dp)) {
-        items(dummyRestaurants) { restaurant ->
-            RestaurantItem(item = restaurant)
+        modifier = Modifier
+            .padding(
+                vertical = 8.dp,
+                horizontal = 8.dp)
+    ) {
+        items(
+            viewModel.state.value
+        ) { restaurant ->
+            RestaurantItem(restaurant) { id ->
+                viewModel.toggleFavorite(id)
+            }
         }
     }
 }
 
 @Composable
-fun RestaurantItem(item: Restaurant) {
-    Card(modifier = Modifier.padding(8.dp)) {
+fun RestaurantItem(
+    item: Restaurant,
+    onClick: (id: Int) -> Unit
+) {
+
+    val icon = if(item.isFavorite)
+        Icons.Filled.Favorite
+    else
+        Icons.Filled.FavoriteBorder
+
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp)
+        ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         ) {
             RestaurantIcon(
                 Icons.Filled.Place,
@@ -44,32 +78,60 @@ fun RestaurantItem(item: Restaurant) {
             RestaurantDetails(
                 item.title,
                 item.description,
-                Modifier.weight(0.85f)
+                Modifier.weight(0.7f)
             )
+            RestaurantIcon(
+                icon,
+                Modifier.weight(0.15f)
+            ) {
+                onClick(item.id)
+            }
         }
     }
 }
 
 @Composable
-fun RestaurantIcon(icon: ImageVector, modifier: Modifier) {
+private fun RestaurantIcon(
+    icon: ImageVector,
+    modifier: Modifier,
+    onClick: () -> Unit = {}
+) {
+
     Image(
         imageVector = icon,
         contentDescription = "Restaurant icon",
-        modifier = Modifier.padding(8.dp))
+        modifier = modifier
+            .padding(8.dp)
+            .clickable { onClick() }
+    )
 }
 
 @Composable
-fun RestaurantDetails(title: String, description: String, modifier: Modifier) {
-    Column(modifier = Modifier) {
+private fun RestaurantDetails(
+    title: String,
+    description: String,
+    modifier: Modifier) {
+
+    Column(modifier = modifier) {
         Text(
             text = title,
-            style = MaterialTheme.typography.titleSmall
+            style = MaterialTheme.typography.titleSmall,
+            maxLines = 1
         )
         Text(
             text = description,
             modifier = Modifier.alpha(0.5f),
-            style = MaterialTheme.typography.bodyMedium
-
+            style = MaterialTheme.typography.bodyMedium,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RestaurantsPreview() {
+    RestaurantTheme {
+        RestaurantsScreen()
     }
 }
